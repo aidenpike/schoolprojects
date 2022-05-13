6a5438a226954d7c55141317a951114b8b77b218
Selisine <Aidenpike113@gmail.com>
2022-05-13 11:29:54 -0400

    Add upgradeTech()

--- Country Battle/Country.h
+++ Country Battle/Country.h
@@ -5,19 +5,22 @@ using namespace std;
 class Country {
     public:
         Country();
-        void getName(string, string);
-        void growTerritories();
-        void upgradeTech();
-        void upgradeArmy();
-        void shrinkTerritories();
-        int returnArmies();
+        void getName(string, string); //Assigns private name vars to names assigned from main to this function
+        void growTerritories(); //+1 Territory for the player if they have an army for it and enough money
+        void upgradeTech(); //+1 on Tech Rating for the player. Buff: Scientists reduce price by 100
+        void upgradeArmy(); //+1 Army for the player if they have enough money 
+        void shrinkTerritories(); //-1 Territory for the player if they have at least 2 territories. Can be sold to opponent
+        int returnArmies(); 
         int returnTerritories();
         int returnTech();
         double returnMoney();
         void lostBattle();
-        void winBattle();
+        void winBattle(); 
+        void addScientist(); //+1 Scientist per 2 Territories. Check each round 
+        void winType(); //How did the player win?
     private:
         int playerOneArmies, playerTwoArmies;
+        int playerOneScientists, playerTwoScientists;
         double playerOneMoney, playerTwoMoney;
         int playerOneTerritories, playerTwoTerritories;
         int playerOneTechnology, playerTwoTechnology;
@@ -27,7 +30,8 @@ class Country {
 //Implementation Section
 Country::Country(){
     playerOneArmies = 10, playerTwoArmies = 10;
-    playerOneMoney = 100000, playerTwoMoney = 100000;
+    playerOneScientists = 2, playerTwoScientists = 2; 
+    playerOneMoney = 100000.0, playerTwoMoney = 100000.0;
     playerOneTerritories = 5, playerTwoTerritories = 5;
     playerOneTechnology = 1, playerTwoTechnology = 1; //x/10 Rating
     playerOneName = "", playerTwoName = "";
@@ -56,5 +60,20 @@ void Country::growTerritories(){
         }
         else
             cout << "You cannot gain another territory!" << endl;
+    }
+}
+
+void Country::upgradeTech(){
+    if (player == 1){
+        if ((playerOneMoney) >= (5000 - (playerOneScientists * 100)) && playerOneTechnology < 10)
+            playerOneTechnology++;
+        else 
+            cout << "You cannot upgrade your technology!";
+    }
+    else if (player == 2){
+        if ((playerTwoMoney) >= (5000 - (playerTwoScientists * 100)) && playerTwoTechnology < 10)
+            playerTwoTechnology++;
+        else 
+            cout << "You cannot upgrade your technology!";
     }
 }
