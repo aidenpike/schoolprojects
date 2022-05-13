b1537c003f2632b7fbec65a99740839a68d2ff49
Selisine <Aidenpike113@gmail.com>
2022-05-13 09:51:22 -0400

    Add Country Constructor

--- Country Battle/Country.h
+++ Country Battle/Country.h
@@ -17,9 +17,18 @@ class Country {
         void lostBattle();
         void winBattle();
     private:
-        int playerOneArmy, playerTwoArmy;
+        int playerOneArmies, playerTwoArmies;
         double playerOneMoney, playerTwoMoney;
-        int playerOneTerritory, playerTwoTerritory;
+        int playerOneTerritories, playerTwoTerritories;
         int playerOneTechnology, playerTwoTechnology;
         string playerOneName, playerTwoName;
+};
+
+//Implementation Section
+Country::Country(){
+    playerOneArmies = 10, playerTwoArmies = 10;
+    playerOneMoney = 100000, playerTwoMoney = 100000;
+    playerOneTerritories = 5, playerTwoTerritories = 5;
+    playerOneTechnology = 1, playerTwoTechnology = 1; //x/10 Rating
+    playerOneName = "", playerTwoName = "";
 }
