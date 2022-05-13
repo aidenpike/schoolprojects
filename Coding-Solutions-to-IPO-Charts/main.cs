56a4284cef22f3f9bb168be52a613f9cd5352a2f
Selisine <Aidenpike113@gmail.com>
2022-05-13 07:38:04 -0400

    Update main.cs

--- Coding-Solutions-to-IPO-Charts/main.cs
+++ Coding-Solutions-to-IPO-Charts/main.cs
@@ -2,12 +2,10 @@ using System;
 
 class Program {
     public static void Main (string[] args) {
-        double beginningBal, depositAmt, withAmt, endBal,
-           envPrice, papPrice, totalPrice,
-           longness, wideness, sqPrice, lwArea, totPrice = 0.0;
-    int envCount, papCount = 0;
-        Console.WriteLine("Below are the programs that solve the 3 scenarios from my first IPO Charts assignments\n");
-        Console.WriteLine("\x1B[32mJohn Lee's Banking");
+        //Scenario 1
+        double beginningBal, depositAmt, withAmt;
+        
+        Console.WriteLine("John Lee's Banking");
         Console.Write("Enter your checking account balance: ");
         beginningBal = Convert.ToDouble(Console.ReadLine());
         Console.Write("Enter the amount deposited during the month: ");
@@ -15,15 +13,15 @@ class Program {
         Console.Write("Enter the withdrawn amount during the month: ");
         withAmt = Convert.ToDouble(Console.ReadLine());
 
-        endBal = beginningBal + depositAmt - withAmt;
+        string output = ((beginningBal + depositAmt - withAmt) < 0) ? "Overdraft!!!\n\n" : "\n";
+        Console.WriteLine("Your balance at the end of the month will be $" + (beginningBal + depositAmt - withAmt));
+        Console.WriteLine(output);
 
-        string overdraft = (endBal < 0) ? "Overdraft!!!\n\n" : "\n";
-
-        Console.WriteLine("Your balance at the end of the month will be $" + endBal);
-        Console.WriteLine(overdraft);
-
-
-        Console.WriteLine("\x1B[32Welcome to the Typing Salon!");
+        //Scenario 2
+        double envPrice, papPrice;
+        int envCount, papCount;
+        
+        Console.WriteLine("Welcome to the Typing Salon!");
         Console.Write("Enter an amount of envelopes: ");
         envCount = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter the price of the envelopes: ");
@@ -32,22 +30,21 @@ class Program {
         papCount = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter the price of the papers: ");
         papPrice = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Your total due is $" + ((envCount * envPrice) + (papCount * papPrice)));
 
-        totalPrice = (envCount * envPrice) + (papCount * papPrice);
-
-        Console.WriteLine("Your total due is $" + totalPrice);
-
-
+        //Scenario 3
+        double length, width, sqPrice;
+        
         Console.WriteLine("\x1B[33m[\x1B[32mThe Limited\x1b[33m]");
         Console.Write("Input length: ");
-        longness = Convert.ToDouble(Console.ReadLine());
+        length = Convert.ToDouble(Console.ReadLine());
         Console.Write("Input width: ");
-        wideness = Convert.ToDouble(Console.ReadLine());
+        width = Convert.ToDouble(Console.ReadLine());
         Console.Write("Enter price per square foot: ");
         sqPrice = Convert.ToDouble(Console.ReadLine());
 
-        lwArea = longness * wideness;
-        totPrice = lwArea * sqPrice;
-        Console.WriteLine("Your total area is" + lwArea + " ft² and your total price is $" + totPrice);
+        
+        Console.WriteLine("Your total area is " + (length * width) + " ft² and your total price is $" + (length * width) * sqPrice);
     }
 }
