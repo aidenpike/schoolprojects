da8104050bbb73d10358d02f7f9b8a55fd284d20
Selisine <Aidenpike113@gmail.com>
2022-05-13 13:05:57 -0400

    Add title screen

--- Country Battle/main.cpp
+++ Country Battle/main.cpp
@@ -5,5 +5,23 @@
 using namespace std;
 
 int main() {
+    //Title Screen
+    cout << "\x1B[31mWWWWWWWW                           WWWWWWWW                                   !!! \n" <<
+                    "W::::::W                           W::::::W                                  !!:!!\n" << 
+                    "W::::::W                           W::::::W                                  !:::!\n" <<
+                    "W::::::W                           W::::::W                                  !:::!\n" << 
+                    " W:::::W           WWWWW           W:::::Waaaaaaaaaaaaa  rrrrr   rrrrrrrrr   !:::!\n" << 
+                    "  W:::::W         W:::::W         W:::::W a::::::::::::a r::::rrr:::::::::r  !:::!\n" <<
+                    "   W:::::W       W:::::::W       W:::::W  aaaaaaaaa:::::ar:::::::::::::::::r !:::!\n" << 
+                    "    W:::::W     W:::::::::W     W:::::W            a::::arr::::::rrrrr::::::r!:::!\n" <<
+                    "     W:::::W   W:::::W:::::W   W:::::W      aaaaaaa:::::a r:::::r     r:::::r!:::!\n" <<
+                    "      W:::::W W:::::W W:::::W W:::::W     aa::::::::::::a r:::::r     rrrrrrr!:::!\n" << 
+                    "       W:::::W:::::W   W:::::W:::::W     a::::aaaa::::::a r:::::r            !!:!!\n" <<
+                    "        W:::::::::W     W:::::::::W     a::::a    a:::::a r:::::r             !!! \n" <<
+                    "         W:::::::W       W:::::::W      a::::a    a:::::a r:::::r                 \n" << 
+                    "          W:::::W         W:::::W       a:::::aaaa::::::a r:::::r             !!! \n" << 
+                    "           W:::W           W:::W         a::::::::::aa:::ar:::::r            !!:!!\n" <<
+                    "            WWW             WWW           aaaaaaaaaa  aaaarrrrrrr             !!! \n" << "\033[0m\n";
+
     
 }
