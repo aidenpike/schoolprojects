e0ece8a17f65b4195b1e991781652bc72132c40a
Selisine <Aidenpike113@gmail.com>
2022-05-13 08:59:22 -0400

    Update main.cpp

--- Making-Change/main.cpp
+++ Making-Change/main.cpp
@@ -3,56 +3,105 @@
 using namespace std;
 
 int main() {
-    int P, Rem, Q, N, Di, Do, amtBack = 0;
-    double amtGive, amtDue = 0.00;
-    
+    int pennies, remainder, quarters, nickles, dimes, amtBack;
+    double amtGive, amtDue;
+
+    //Scenario 1
     cout << "Scenario 1\n";
 
     cout << "Input amount of pennies: ";
-    cin >> P;
-    cout << "You have " << P/100 << " dollar(s), ";
+    cin >> pennies;
+    
+    if (pennies/100 == 1)
+        cout << "You get back " << pennies/100 << " dollar, ";
+    else
+        cout << "You get back " << pennies/100 << " dollars, "; 
+        
+        remainder = pennies % 100;
+        quarters = remainder / 25;
+        
+        if (quarters == 1)
+            cout << quarters << " quarter, ";
+        else
+            cout << quarters << " quarters, ";
+        
+        remainder = remainder % 25;
+        dimes = remainder / 10;
+        
+        if (dimes == 1)
+            cout << dimes << " dime, ";
+        else
+            cout << dimes << " dimes, ";
+        
+        remainder = remainder % 10;
+        nickles = remainder / 5;
 
-    Rem = P % 100; Q = Rem/25;
-    cout << Q << " quarter(s), ";
+        if (nickles == 1)
+            cout << nickles << " nickel, ";
+        else
+            cout << nickles << " nickles, ";
+        
+        remainder = remainder % 5;
 
-    Rem = Rem % 25; Di = Rem/10;
-    cout << Di << " dime(s), ";
+        if (remainder == 1)
+            cout << "and " << remainder << " penny." << endl;
+        else
+            cout << "and " << remainder << " pennies." << endl;
 
-    Rem = Rem % 10; N = Rem/5;
-    cout << N << " nickel(s), ";
-
-    Rem = Rem % 5;
-    cout << " and " << Rem << " penny(ies).\n \n";
-
-
-    cout << "Number 2 \n";
+    //Scenario 2
+    cout << "Scenario 2 \n";
 
     cout << "Amount due: ";
     cin >> amtDue;
+    
     amtDue = amtDue * 100;
-
-    cout << "Pay here: ";
-    cin >> amtGive;
-    amtGive = amtGive * 100;
-
-    amtBack = amtGive - amtDue;
-
-    if (amtBack < 0)
-        cout << "Invalid! Payment does not reach required amount due.\n";
-    else {
-  
-    cout << "You get back " << amtBack/100 << " dollar(s), ";
-    Rem = amtBack % 100;
-
-    Q = Rem / 25;
-    cout << Q << " quarter(s), ";
-    Rem = Rem % 25;
-    Di = Rem / 10;
-    cout << Di << " dime(s), ";
-    Rem = Rem % 10;
-    N = Rem / 5;
-    cout << N << " nickel(s), ";
-    Rem = Rem % 5;
-    cout << " and " << Rem << " penny(ies)." << endl;
+    amtBack = -1;
+    
+    while (amtBack < 0){
+        cout << "Pay here: ";
+        cin >> amtGive;
+        amtGive = amtGive * 100;
+    
+        amtBack = amtGive - amtDue;
+    
+        if (amtBack < 0)
+            cout << "Invalid! Payment does not reach required amount due.\n";
+        else {
+            if (amtBack/100 == 1)
+                cout << "You get back " << amtBack/100 << " dollar, ";
+            else
+                cout << "You get back " << amtBack/100 << " dollars, "; 
+            
+            remainder = amtBack % 100;
+            quarters = remainder / 25;
+            
+            if (quarters == 1)
+                cout << quarters << " quarter, ";
+            else
+                cout << quarters << " quarters, ";
+            
+            remainder = remainder % 25;
+            dimes = remainder / 10;
+            
+            if (dimes == 1)
+                cout << dimes << " dime, ";
+            else
+                cout << dimes << " dimes, ";
+            
+            remainder = remainder % 10;
+            nickles = remainder / 5;
+    
+            if (nickles == 1)
+                cout << nickles << " nickel, ";
+            else
+                cout << nickles << " nickles, ";
+            
+            remainder = remainder % 5;
+    
+            if (remainder == 1)
+                cout << "and " << remainder << " penny." << endl;
+            else
+                cout << "and " << remainder << " pennies." << endl; 
+        }
     }
 }
