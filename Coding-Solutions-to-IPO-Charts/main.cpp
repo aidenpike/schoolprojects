3280845256147da31105313dc7cd05d29d36e39f
Selisine <Aidenpike113@gmail.com>
2022-05-12 20:06:23 -0400

    Move variables, remove unneeded variables, reformat code

--- Coding-Solutions-to-IPO-Charts/main.cpp
+++ Coding-Solutions-to-IPO-Charts/main.cpp
@@ -4,37 +4,42 @@ using namespace std;
 
 int main() 
 {
-    double beginningBal, depositAmt, withAmt, endBal,
-           envPrice, papPrice, totalPrice,
-           longness, wideness, sqPrice, lwArea, totPrice = 0.0;
-    int envCount, papCount = 0;
-
-    cout << "Below are the programs that solve the 3 scenarios from my first IPO Charts assignments\n\n";
+    //Scenario 1
+    double beginningBal, depositAmt, withAmt, endBal;
     
     cout << "\x1B[32mJohn Lee's Banking\033[0m\n"; 
-    cout << "First enter your checking account balance from the beginning of the month, then the amount deposited during the month, then the withdrawn amount during the month:\n";
-    cin >> beginningBal >> depositAmt >> withAmt; 
+    cout << "Enter your checking account balance from the beginning of the month: "; 
+    cin >> beginningBal;
+    cout << "Enter the amount deposited during the month: ";
+    cin >> depositAmt;
+    cout << "Enter the withdrawn amount during the month: ";
+    cin >> withAmt; 
     
-    endBal = beginningBal + depositAmt - withAmt;
+    string overdraft = ((beginningBal + depositAmt - withAmt) < 0) ? "Overdraft!!!\n\n" : "\n";
     
-    string overdraft = (endBal < 0) ? "Overdraft!!!\n\n" : "\n";
-    
-    cout << "Your balance at the end of the month will be $" << endBal << endl << endl;
+    cout << "Your balance at the end of the month will be $" << (beginningBal + depositAmt - withAmt) << endl << endl;
     cout << overdraft;
+
+    //Scenario 2
+    double envPrice, papPrice, totalPrice;
+    int envCount, papCount = 0;
     
     cout << "\x1B[32mWelcome to the Typing Salon!\033[0m\n";
-    cout << "Enter the amount of envelopes, their price, then the amount of papers, then their price:\n";
-    cin >> envCount >> envPrice >> papCount >> papPrice;
-    totalPrice = (envCount * envPrice) + (papCount * papPrice);
+    cout << "Enter the amount of envelopes and their price: ";
+    cin >> envCount >> envPrice;
+    cout << "Enter the amount of papers and their price: ";
+    cin >> papCount >> papPrice;
   
-    cout << "Your total due is $" << totalPrice << endl << endl;
-  
+    cout << "Your total due is $" << (envCount * envPrice) + (papCount * papPrice) << endl << endl;
+
+    //Scenario 3
+    double length, width;
+    double pricePerSqFt;
+    
     cout << "\x1B[33m[\033\x1B[32mThe Limited\033[0m\x1B[33m]\033[0m\n";
   
     cout << "Input length, then width, then price per square foot:\n";
-    cin >> longness >> wideness >> sqPrice;
-    
-    lwArea = longness * wideness, totPrice = lwArea * sqPrice;
-    
-    cout << "Your total area is " << lwArea << " ft² and your total price is $" << totPrice << endl;
+    cin >> length >> width >> pricePerSqFt;
+
+    cout << "Your total area is " << length * width << " ft² and your total price is $" << (length * width) * pricePerSqFt << endl;
 }
