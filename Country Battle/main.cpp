ab141a43e2d16911cb142bb41e1435f0456947df
Selisine <Aidenpike113@gmail.com>
2022-05-18 16:47:06 -0400

    Various changes see desc
    Add playAgain feature
    Put in hasWon loop and playAgain loop
    Add playerOne choices 
    Code playerOne choices 1-4 and 6-7
    addScientist() added to end of hasWon loop
    Add returning stats of player at the end of their turn for playerOne
    Add displaying stats for both players at the beginning
    Add name input
    Move name input to Country.h
    Add "press enter to continue" due to the cin.ignore() required

--- Country Battle/main.cpp
+++ Country Battle/main.cpp
@@ -5,23 +5,94 @@
 using namespace std;
 
 int main() {
-    //Title Screen
-    cout << "\x1B[31mWWWWWWWW                           WWWWWWWW                                   !!! \n" <<
-                    "W::::::W                           W::::::W                                  !!:!!\n" << 
-                    "W::::::W                           W::::::W                                  !:::!\n" <<
-                    "W::::::W                           W::::::W                                  !:::!\n" << 
-                    " W:::::W           WWWWW           W:::::Waaaaaaaaaaaaa  rrrrr   rrrrrrrrr   !:::!\n" << 
-                    "  W:::::W         W:::::W         W:::::W a::::::::::::a r::::rrr:::::::::r  !:::!\n" <<
-                    "   W:::::W       W:::::::W       W:::::W  aaaaaaaaa:::::ar:::::::::::::::::r !:::!\n" << 
-                    "    W:::::W     W:::::::::W     W:::::W            a::::arr::::::rrrrr::::::r!:::!\n" <<
-                    "     W:::::W   W:::::W:::::W   W:::::W      aaaaaaa:::::a r:::::r     r:::::r!:::!\n" <<
-                    "      W:::::W W:::::W W:::::W W:::::W     aa::::::::::::a r:::::r     rrrrrrr!:::!\n" << 
-                    "       W:::::W:::::W   W:::::W:::::W     a::::aaaa::::::a r:::::r            !!:!!\n" <<
-                    "        W:::::::::W     W:::::::::W     a::::a    a:::::a r:::::r             !!! \n" <<
-                    "         W:::::::W       W:::::::W      a::::a    a:::::a r:::::r                 \n" << 
-                    "          W:::::W         W:::::W       a:::::aaaa::::::a r:::::r             !!! \n" << 
-                    "           W:::W           W:::W         a::::::::::aa:::ar:::::r            !!:!!\n" <<
-                    "            WWW             WWW           aaaaaaaaaa  aaaarrrrrrr             !!! \n" << "\033[0m\n";
+    Country playerOne;
+    Country playerTwo;
+    Country misc;
+    char playAgain = 'Y';
+    int playerChoice;
+    bool hasWon = false;
 
+    //Title Screen
+        cout << "\x1B[31mWWWWWWWW                           WWWWWWWW                                   !!! \n" <<
+                        "W::::::W                           W::::::W                                  !!:!!\n" << 
+                        "W::::::W                           W::::::W                                  !:::!\n" <<
+                        "W::::::W                           W::::::W                                  !:::!\n" << 
+                        " W:::::W           WWWWW           W:::::Waaaaaaaaaaaaa  rrrrr   rrrrrrrrr   !:::!\n" << 
+                        "  W:::::W         W:::::W         W:::::W a::::::::::::a r::::rrr:::::::::r  !:::!\n" <<
+                        "   W:::::W       W:::::::W       W:::::W  aaaaaaaaa:::::ar:::::::::::::::::r !:::!\n" << 
+                        "    W:::::W     W:::::::::W     W:::::W            a::::arr::::::rrrrr::::::r!:::!\n" <<
+                        "     W:::::W   W:::::W:::::W   W:::::W      aaaaaaa:::::a r:::::r     r:::::r!:::!\n" <<
+                        "      W:::::W W:::::W W:::::W W:::::W     aa::::::::::::a r:::::r     rrrrrrr!:::!\n" << 
+                        "       W:::::W:::::W   W:::::W:::::W     a::::aaaa::::::a r:::::r            !!:!!\n" <<
+                        "        W:::::::::W     W:::::::::W     a::::a    a:::::a r:::::r             !!! \n" <<
+                        "         W:::::::W       W:::::::W      a::::a    a:::::a r:::::r                 \n" << 
+                        "          W:::::W         W:::::W       a:::::aaaa::::::a r:::::r             !!! \n" << 
+                        "           W:::W           W:::W         a::::::::::aa:::ar:::::r            !!:!!\n" <<
+                        "            WWW             WWW           aaaaaaaaaa  aaaarrrrrrr             !!!\033[0m\n" << 
+                        "Press enter to continue. "                                                            << endl;
+    do {
+        cin.ignore();
+        misc.getName();
+
+        cout << playerOne.returnName(1) << " you have " << playerOne.returnArmies(1) << " Armies, " << playerOne.returnTerritories(1) << " Territories, Technology Level " << playerOne.returnTech(1) << "/10, and $" << playerOne.returnMoney(1) << endl;
+            cout << playerTwo.returnName(2) << " you now " << playerTwo.returnArmies(2) << " Armies, " << playerTwo.returnTerritories(2) << " Territories, Technology Level " << playerTwo.returnTech(2) << "/10, and $" << playerTwo.returnMoney(2) << endl;
+        
+        do {
+            cout << playerOne.returnName(1) << ", it is your turn. What do you wish to do?" << endl <<
+                                               "\t1. Grow Territories ($10000 [Requires a spare army to control])\n" <<
+                                               "\t2. Shrink Territories (Get back $10000 [Can sell to " << playerTwo.returnName(2) << "])\n" << 
+                                               "\t3. Upgrade Technology ($" << 5000 - (playerOne.returnScientists(1) * 100) << " [Original Price: $5000])\n" <<
+                                               "\t4. Upgrade Army ($8000)\n" <<
+                                               "\t5. Attack\n" <<
+                                               "\t6. Pass\n" <<
+                                               "\t7. Forfeit\n";
+            cin >> playerChoice;
+            
+            switch (playerChoice){
+                case 1:
+                    playerOne.growTerritories(1);
+                break;
+                
+                case 2:
+                    playerOne.shrinkTerritories(' ', ' ', 1);
+                break;
     
+                case 3:
+                    playerOne.upgradeTech(1);
+                break;
+    
+                case 4:
+                    playerOne.upgradeArmy(1);
+                break;
+    
+                case 5:
+                    //Attack action to be coded in main
+                break;
+    
+                case 6:
+                    cout << playerOne.returnName(1) << " has decided to pass this round." << endl;
+                break;
+    
+                case 7:
+                    playerTwo.winType(2, "forfeit");
+                    hasWon = true;
+                break;
+    
+                default:
+                break;
+            } 
+
+            cout << playerOne.returnName(1) << " you now have " << playerOne.returnArmies(1) << " Armies, " << playerOne.returnTerritories(1) << " Territories, Technology Level " << playerOne.returnTech(1) << "/10, and $" << playerOne.returnMoney(1) << endl;
+            misc.addScientist();
+        } while (!hasWon);
+        
+            //Play Again
+            cout << "Would you like to play again? [Y or N]: ";
+            cin >> playAgain;
+            playAgain = toupper(playAgain); 
+    
+            cin.ignore();
+    } while (playAgain == 'Y');
+    
+    cout << "Goodbye!";
 }
