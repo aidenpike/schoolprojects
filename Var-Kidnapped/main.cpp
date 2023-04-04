f9506df8f2baf5e345bbc889f3b5b28d3c1ea359
Selisine <Aidenpike113@gmail.com>
2023-04-04 11:49:22 -0400

    fix all of var kidnapped
    I'm sorry the formatting was foul

--- Var-Kidnapped/main.cpp
+++ Var-Kidnapped/main.cpp
@@ -1,69 +1,104 @@
 #include <iostream>
 #include <cmath> 
-#include <string>
-
-using namespace std;
 
 int main() {
-    double l, wi,
-           cot, mpg,
-           pay, hours, hwage,
-           cpg, gallons, cost, roundcost,
-           h, m = 0.0;
-    string name = " ";
-    int    s, n, totS = 0;
+    int length;
+    int width;
 
-    cout << "Scenario 1: Abduction!" << endl;
-    cout << "Input length and width: ";
-    cin >> l >> wi;    
-    cout << "Your total area is " << l*wi << " and your total perimeter is " << (l+wi)*2 << endl << endl;
+    //Scenario 1
+    std::cout << "Scenario 1: Abduction!\n";
+    std::cout << "Enter length: ";
+    std::cin >> length;
+    std::cout << "Enter width: ";
+    std::cin >> width;
 
+    int area = length * width;
+
+    std::cout << "Your total area is " << area << " and your total perimeter is " << area * 2 << "\n\n";
+
+    //Scenario 2
+    double tankCapacity;
+    double milePerGallon;
+
+    std::cout << "Scenario 2: Find the right car!\n";
+    std::cout << "Enter the capacity of the gas tank in gallons: ";
+    std::cin >> tankCapacity;
+    std::cout << "Enter the mile per gallon: ";
+    std::cin >> milePerGallon; 
+    std::cout << "You can drive for " << tankCapacity * milePerGallon << " miles without refueling.\n\n";
+
+    //Scenario 3
+    double payRecieved;
+    int hoursWorked;
+    std::string name;
+
+    std::cout << "Scenario 3: Getting a minimum wage job!\n";
+    std::cout << "Input your pay recieved: ";
+    std::cin >> payRecieved;
+    std::cout << "Enter your hours worked: ";
+    std::cin >> hoursWorked;
+    std::cin.ignore();
+    std::cout << "Input your name: ";
+    std::getline(std::cin, name);
     
-    cout << "Scenario 2: Find the right car!" << endl;
-    cout << "Enter the capacity of the gas tank in gallons, as well as the MPG: ";
-    cin >> cot >> mpg;
-    cout << "You can drive for " << cot * mpg << " miles without refueling.\n\n";
-
-    
-    cout << "Scenario 3: Getting a minimum wage job!" << endl;
-    cout << "Input your pay recieved and hours worked, as well as your name: ";
-    cin >> pay >> hours >> name;
-    if ((pay/hours) == 1)
-        cout << name << ", your hourly wage is " << pay/hours << " dollar an hour.\n\n";
+    if ((payRecieved/hoursWorked) == 1)
+        std::cout << name << ", your hourly wage is " << payRecieved/hoursWorked << " dollar an hour.\n\n";
     else
-        cout << name << ", your hourly wage is " << pay/hours << " dollars an hour.\n\n";
-    
-    cout << "Scenario 4: Buying gas!" << endl;
-    cout << "Enter in cost per gallon and amount of gallons: ";
-    cin >> cpg >> gallons;
-    cout << "Cost: " << cpg * gallons << endl;
-    cout << "Rounded Cost: " << round(cpg*gallons) << endl << endl;
+        std::cout << name << ", your hourly wage is " << payRecieved/hoursWorked << " dollars an hour.\n\n";
 
+    //Scenario 4
+    double costPerGallon;
+    double gallons;
     
-    cout << "Scenario 5: IT'S TIME TO DU-DU-DU-DU-DU DUEL!" << endl;
-    cout << "Input hours, minutes, and seconds: ";
-    cin >> h >> m >> s;
-    totS = (h*3600) + (m*60) + s;
-    if (totS == 1)
-        cout << "That is " << (h*3600) + (m*60) + s << " second.\n\n";
+    std::cout << "Scenario 4: Buying gas!\n";
+    std::cout << "Enter in cost per gallon: ";
+    std::cin >> costPerGallon;
+    std::cout << "Enter amount of gallons: ";
+    std::cin >> gallons;
+    std::cout << "Cost: " << costPerGallon * gallons << "\n";
+    std::cout << "Rounded Cost: " << round(costPerGallon * gallons) << "\n\n";
+
+    //Scenario 5
+    int hours;
+    int minutes;
+    int seconds;
+    
+    std::cout << "Scenario 5: IT'S TIME TO DU-DU-DU-DU-DU DUEL!\n";
+    std::cout << "Enter hours: ";
+    std::cin >> hours;
+    std::cout << "Enter minutes: ";
+    std::cin >> minutes;
+    std::cout << "Enter seconds: ";
+    std::cin >> seconds;
+    
+    int toSeconds = (hours*3600) + (minutes*60) + seconds;
+    
+    if (toSeconds == 1)
+        std::cout << "That is " << toSeconds << " second.\n\n";
     else
-        cout << "That is " << (h*3600) + (m*60) + s << " seconds.\n\n";
+        std::cout << "That is " << toSeconds << " seconds.\n\n";
 
-
-    cout << "Scenario 6: ...ugh!" << endl;   
-    cout << "Enter how many Ninja Turtle Action Figures there are: ";
-    cin >> n;   
-    cout << "Crates needed: " << n/20 << endl << "Leftover Turtles: " << n%20 << endl << endl;
-
+    //Scenario 6
+    int actionFigures;
     
-    cout << "Scenario 7: It's time to go." << endl;
+    std::cout << "Scenario 6: ...ugh!\n";
+    std::cout << "Enter how many Ninja Turtle Action Figures there are: ";
+    std::cin >> actionFigures;
+    std::cout << "Crates needed: " << actionFigures / 20 << "\nLeftover Turtles: " << actionFigures % 20 << "\n";
 
-    h = 0; m = 0; s = 0;
-    
-    cout << "Enter in amount of seconds: ";
-    cin >> s;
-   
-    h = s/3600; m = (s % 3600) / 60; s = s % 3600 % 60;
+    //Scenario 7
+    std::cout << "Scenario 7: It's time to go.\n";
 
-    cout << "That is " << h << " hours, " << m << " minutes, and " << s << " second(s)." << endl;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    std::cout << "Enter in amount of seconds: ";
+    std::cin >> seconds;
+
+    hours = seconds / 3600;
+    minutes = (seconds % 3600) / 60;
+    seconds = seconds % 3600 % 60;
+
+    std::cout << "That is " << hours << " hour(s), " << minutes << " minute(s), and " << seconds << " second(s).\n";
 }
